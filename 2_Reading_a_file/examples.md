d0fedce25cf86d503df55328fe6fff10dc903f7c
Andrew Graves <agraves@codecademy.com>
2023-08-23 16:25:10 -0400

    WIP moving onto lesson 2

---
+++ 2_Reading_a_file/examples.md
@@ -0,0 +1,46 @@
+Reading a file (always raw bytes)
+
+```go
+rawBytes, err := os.ReadFile("./path/to/file.whatever")
+```
+
+Unmarshalling JSON (slice of bytes in JSON -> go struct)
+
+Note: fields MUST be uppercased for the unmarshalling
+
+TODO: aliasing vs default
+
+```go
+rawData := []bytes{`{"numberField":1, "booleanField":false}`}
+
+type SomeStruct struct {
+	AliasOne int `json:"numberField"`
+	AliasTwo bool `json:"booleanField"`
+}
+
+targetInstance := SomeStruct{}
+finalStruct, err := json.Unmarshal(rawData, &targetInstance)
+```
+
+Structs can be nested and their json aliases
+
+```go
+type author struct {
+	FirstName string `json:"first_name"`
+	LastName string `json:"last_name"`
+}
+
+type Book struct {
+	Title string // no need to alias if field names match
+	Author author
+}
+/* json equivalent:
+	{
+		"title": "something",
+		"author": {
+			"first_name": "Bob",
+			"last_name": "Jones"
+		}
+	}
+*/
+```
--- solutions/2_Reading_a_file/main.go
+++ solutions/2_Reading_a_file/main.go
@@ -24,9 +24,63 @@ func handleRoot(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(json))
 }
 
+type furColor struct {
+	Primary    string
+	Highlights string
+}
+
+type coordinates struct {
+	Lat float64
+	Lng float64
+}
+
+type SquirrelSighting struct {
+	SquirrelID             string   `json:"Squirrel_ID"`
+	FurColor               furColor `json:"Fur_Color"`
+	Park                   string
+	InteractionsWithHumans string `json:"Interactions_With_Humans"`
+	Activities             []string
+	Coorditantes           coordinates
+}
+
+func handleSquirrels(w http.ResponseWriter, r *http.Request) {
+	fileContents, err := os.ReadFile("../../static/data.json")
+	if err != nil {
+		panic(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(fileContents)
+}
+
+func handleSquirrelByID(w http.ResponseWriter, r *http.Request) {
+	fileContents, err := os.ReadFile("../../static/data.json")
+	if err != nil {
+		panic(err)
+	}
+	list := []SquirrelSighting{}
+	err = json.Unmarshal(fileContents, list)
+	if err != nil {
+		panic(err)
+	}
+
+	first := list[0]
+	resp, err := json.Marshal(first)
+	if err != nil {
+		panic(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(resp)
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/", http.HandlerFunc(handleRoot))
+	mux.Handle("/squirrels", http.HandlerFunc(handleSquirrels))
+	// TODO: we can't handle this in standard lib alone:
+	// maybe we switch to using gin framework?
+	mux.Handle("/squirrels/:id", http.HandlerFunc(handleSquirrels))
 
-	http.ListenAndServe("localhost:3000", mux)
+	http.ListenAndServe("localhost:4321", mux)
 }
